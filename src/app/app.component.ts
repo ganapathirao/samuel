6b67e9162028716e0787c319885afd9b478d4734
samuelrajsams <samuelrajsams@gmail.com>
2018-12-26 13:51:18 +0530

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ declare var google: any;
 export class AppComponent implements OnInit {
   title = 'app';
   bikeDetails: any;
+  headingOfCrimeRateIndex: any;
   riskIndex: any;
   uluru: Object = { lat: -25.363, lng: 131.044 };
   map: Object;
@@ -18,41 +19,51 @@ export class AppComponent implements OnInit {
   zoom: number;
   @ViewChild('map') mapRef: ElementRef;
   constructor(
-    private authenticationService: DataService) {
+    private dataService: DataService) {
 
   }
   ngOnInit() {
     this.getTransactionDetails('clrsearch?');
     this.getScore('walkscore?');
-    this.googleMapsIntialize();
+    //this.googleMapsIntialize();
   }
 
   ngAfterViewInit() {
-    this.map = new google.maps.Map(this.mapRef.nativeElement, {
-      zoom: 4,
-      center: this.uluru
-    });
-    this.marker = new google.maps.Marker({
-      position: this.uluru,
-      map: this.map
-    });
+    setTimeout(() => {
+      var fenway = { lat: 42.345573, lng: -71.098326 };
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: fenway,
+        zoom: 14
+      });
+      var panorama = new google.maps.StreetViewPanorama(
+        document.getElementById('pano'), {
+          position: fenway,
+          pov: {
+            heading: 34,
+            pitch: 10
+          }
+        });
+      map.setStreetView(panorama);
+    }, 2000);
+
+    // setTimeout(() => {
+    //   this.map = new google.maps.Map(this.mapRef.nativeElement, {
+    //     zoom: 4,
+    //     center: this.uluru
+    //   });
+    //   this.marker = new google.maps.Marker({
+    //     position: this.uluru,
+    //     map: this.map
+    //   });
+    // }, 2000)
+
   }
 
+  /**Google maps intilaizing
+   * 
+   */
   googleMapsIntialize() {
-    var fenway = { lat: 42.345573, lng: -71.098326 };
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: fenway,
-      zoom: 14
-    });
-    var panorama = new google.maps.StreetViewPanorama(
-      document.getElementById('pano'), {
-        position: fenway,
-        pov: {
-          heading: 34,
-          pitch: 10
-        }
-      });
-    map.setStreetView(panorama);
+
   }
 
 
@@ -61,10 +72,15 @@ export class AppComponent implements OnInit {
    * @param url 
    */
   getTransactionDetails(url: string) {
-    this.authenticationService.getData(url + "transaction_uid=" + CommonConstants.transaction_uid + "&" + "property_uid=" + CommonConstants.property_uid).subscribe((response: any) => {
+    this.dataService.getData(url + "transaction_uid=" + CommonConstants.transaction_uid + "&" + "property_uid=" + CommonConstants.property_uid).subscribe((response: any) => {
       if (response.status) {
+        if (response.crime_index_data.heading.length > 0) {
+          this.headingOfCrimeRateIndex = response.crime_index_data.heading
+        } else {
+          this.headingOfCrimeRateIndex = [];
+        }
         if (response.crime_index_data.rows.length > 0) {
-          let objs = response.crime_index_data.rows.map(function (x) {
+          let objs = response.crime_index_data.rows.map(function (x: any) {
             return {
               clrIndex: x[0].toString(),
               cleveland: x[1].toString(),
@@ -87,15 +103,16 @@ export class AppComponent implements OnInit {
    * @param url 
    */
   getScore(url: string) {
-    this.authenticationService.getData(url + "transaction_uid=" + CommonConstants.transaction_uid + "&" + "property_uid=" + CommonConstants.property_uid).subscribe((response: any) => {
+    this.dataService.getData(url + "transaction_uid=" + CommonConstants.transaction_uid + "&" + "property_uid=" + CommonConstants.property_uid).subscribe((response: any) => {
       if (response.status) {
-        //console.log(response);
+        console.log(response);
         if (response.bike) {
           this.bikeDetails = response.bike;
         } else {
           this.bikeDetails = [];
         }
       } else {
+        console.log("status not found");
       }
     });
   }
